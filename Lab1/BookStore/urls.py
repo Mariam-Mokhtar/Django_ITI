3f6b18d4bc8124d62826c9bea6eb1114c57f0fee
Mariam-Mokhtar <mariamreda1819@gmail.com>
2023-05-14 14:04:11 +0300

    first commit

---
+++ Lab1/BookStore/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import index, books_list,book_details,book_delete,book_update
+
+app_name = 'books'
+
+urlpatterns = [
+    path('', index, name='bookStore-index'),
+    path('books_list/', books_list, name="books_list"),
+    path('book_details/<int:book_id>', book_details, name="book_details"),
+    path('book_delete/<int:task_id>', book_delete, name="book_delete"),
+    path('book_update/<int:task_id>', book_update, name="book_update")
+]
