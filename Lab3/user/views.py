c599c01d18fbceb74dad3e52c61c55e1a7b23137
Mariam-Mokhtar <mariamreda1819@gmail.com>
2023-06-08 21:01:52 +0300

    Lab3

---
+++ Lab3/user/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render , redirect
+from django.contrib import messages
+from django.contrib.auth import login , logout , authenticate
+from .forms import LoginForm , SignUpForm
+from django.views.decorators.csrf import csrf_protect
+
+
+def user_login(request):
+    form = LoginForm()
+    if request.user.is_authenticated:
+        return redirect("BookStore:home-index")
+
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is None:
+            return render(request, "accounts/login.html", {"form": form})
+        login(request , user)
+        return redirect("BookStore:home-index")
+    else:
+        return render(request, "accounts/login.html" , {"form": form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect("user:login")
+
+@csrf_protect
+def user_signup(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect("user:login")
+        else:
+           errors = form.errors
+           return render(request, "accounts/signup.html" , {"form": form , "errors": errors}) 
+    else:
+        return render(request, "accounts/signup.html" , {"form": form})
