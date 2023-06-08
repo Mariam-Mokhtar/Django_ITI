d4d4d1018c604066f078252d8a7827b7f40c03ed
Mariam Reda <83876328+Mariam-Mokhtar@users.noreply.github.com>
2023-06-08 21:17:55 +0300

    Delete Movie directory

--- Movie/home/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import movie_list, movie_detail
-
-app_name = "home-api"
-
-urlpatterns = [
-    path('movies/', movie_list, name='movie_list'),
-    path('movies/<int:pk>/', movie_detail, name='movie_detail'),
-]
